7b16236f391f47ca72dfed33d9f319d8993fd3c0
hardplush <hardplush@gmail.com>
2024-05-21 07:41:08 +0700

    added toolbar and navigation

--- app/src/main/java/com/hardplush/filmsearch/MainActivity.kt
+++ app/src/main/java/com/hardplush/filmsearch/MainActivity.kt
@@ -8,29 +8,43 @@ import com.hardplush.filmsearch.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initButtons()
+        initNavigation()
     }
-    private fun initButtons() {
-        binding.buttonMenu.setOnClickListener() {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+
+    private fun initNavigation() {
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
-        binding.buttonFav.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                    R.id.favorites -> {
+                        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.watch_later -> {
+                        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.selections -> {
+                        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
         }
-        binding.buttonWatchLater.setOnClickListener {
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonSelections.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonSetting.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
+
     }
-}
