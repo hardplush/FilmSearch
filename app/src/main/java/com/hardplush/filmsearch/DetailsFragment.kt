c9183633a4b0ab1feb75b545c8b58ceb66cdec3f
hardplush <hardplush@gmail.com>
2024-06-18 20:36:00 +0700

    adding activity

---
+++ app/src/main/java/com/hardplush/filmsearch/DetailsFragment.kt
@@ -0,0 +1,39 @@
+package com.hardplush.filmsearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hardplush.filmsearch.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+        val film = arguments?.get("film") as Film
+
+        binding.detailsToolbar.title = film.title
+        binding.detailsPoster.setImageResource(film.poster)
+        binding.detailsDescription.text = film.description
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
